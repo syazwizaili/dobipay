3e1b5b34ed2e7acee7aba4089365b1bdde328239
Syazwi <>
2025-07-03 02:24:09 +0800

    update pwa setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import MyApp from "./app";
-import NextTopLoader from 'nextjs-toploader';
+import NextTopLoader from "nextjs-toploader";
 import "./global.css";
-import { CustomizerContextProvider } from "./context/customizerContext";
-
+import {CustomizerContextProvider} from "./context/customizerContext";
 
 export const metadata = {
-  title: "Modernize Main Demo",
-  description: "Modernize Main kit",
+  title: "DobiPay",
+  description: "DobiPay - Your Laundry Payment Solution",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#fff" />
+      </head>
       <body>
         <NextTopLoader color="#5D87FF" />
         <CustomizerContextProvider>
